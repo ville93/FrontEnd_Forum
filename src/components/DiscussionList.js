50422f465d193eb6e1305d828ebbebbbee7f2d81
Ville Ritala <ritalaville@gmail.com>
2024-02-18 12:06:36 +0200

    useEffect added to DiscussionList for error handling

--- src/components/DiscussionList.js
+++ src/components/DiscussionList.js
@@ -1,26 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { List, ListItem, Typography, Paper, Grid, Link } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { List, ListItem, Typography, Paper, Grid, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const DiscussionList = ({ endpoint }) => {
   const [discussions, setDiscussions] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch(`https://localhost:5001${endpoint}`)
-      .then(response => response.json())
-      .then(data => setDiscussions(data))
-      .catch(error => console.error('Error fetching discussions:', error));
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://localhost:5001${endpoint}`);
+        if (!response.ok) {
+          throw new Error("Error fetching discussions");
+        }
+        const data = await response.json();
+        setDiscussions(data);
+      } catch (error) {
+        console.error("Error fetching discussions:", error);
+      }
+    };
+    fetchData();
   }, [endpoint]);
 
   const navigateToDiscussionPage = (discussionId) => {
-    console.log(discussionId)
+    console.log(discussionId);
     navigate(`/discussion/${discussionId}`);
   };
 
   return (
     <div>
-      <Grid container spacing={2} alignItems="center" >
+      <Grid container spacing={2} alignItems="center">
         <Grid item xs={8}>
           <Typography>Topic</Typography>
         </Grid>
@@ -32,23 +41,46 @@ const DiscussionList = ({ endpoint }) => {
         </Grid>
       </Grid>
       <List>
-        {discussions.map(discussion => (
+        {discussions.map((discussion) => (
           <ListItem key={discussion.id}>
-            <Paper style={{ padding: '10px', background: '#313331', width: '100%' }}>
-              <Grid container spacing={2} alignItems="center" style={{ marginLeft: 'auto', marginRight: 'auto' }}> 
+            <Paper
+              style={{ padding: "10px", background: "#313331", width: "100%" }}
+            >
+              <Grid
+                container
+                spacing={2}
+                alignItems="center"
+                style={{ marginLeft: "auto", marginRight: "auto" }}
+              >
                 <Grid item xs={8}>
                   <Link onClick={() => navigateToDiscussionPage(discussion.id)}>
-                    <Typography style={{ fontWeight: 'bold', color: '#4CAF50' }}>
+                    <Typography
+                      style={{ fontWeight: "bold", color: "#4CAF50" }}
+                    >
                       {discussion.title}
                     </Typography>
                   </Link>
-                  <Typography style={{ fontWeight: 'bold', color: '#ffffff' }}>{discussion.messages[0].content}</Typography>
+                  {discussion.messages && discussion.messages.length > 0 && (
+                    <Typography
+                      style={{ fontWeight: "bold", color: "#ffffff" }}
+                    >
+                      {discussion.messages[0].content}
+                    </Typography>
+                  )}
                 </Grid>
                 <Grid item xs={2}>
-                  <Typography style={{ fontWeight: 'bold', color: '#ffffff' }}>{discussion.answersCount}</Typography>
+                  <Typography style={{ fontWeight: "bold", color: "#ffffff" }}>
+                    {discussion.answersCount}
+                  </Typography>
                 </Grid>
                 <Grid item xs={2}>
-                  <Typography style={{ fontWeight: 'bold', color: '#ffffff' }}>{formatDate(discussion.messages[0].time)}</Typography>
+                  {discussion.messages && discussion.messages.length > 0 && (
+                    <Typography
+                      style={{ fontWeight: "bold", color: "#ffffff" }}
+                    >
+                      {formatDate(discussion.messages[0].time)}
+                    </Typography>
+                  )}
                 </Grid>
               </Grid>
             </Paper>
@@ -60,8 +92,14 @@ const DiscussionList = ({ endpoint }) => {
 };
 
 function formatDate(dateString) {
-  const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-  return new Date(dateString).toLocaleDateString('fi-FI', options);
+  const options = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  return new Date(dateString).toLocaleDateString("fi-FI", options);
 }
 
 export default DiscussionList;
