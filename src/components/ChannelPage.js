5361d77cf0f3fbc07ec1f920d52aa7c8c15edc3e
Ville Ritala <ritalaville@gmail.com>
2024-02-18 11:43:44 +0200

    Navigating to the Discussion from ChannelPage

--- src/components/ChannelPage.js
+++ src/components/ChannelPage.js
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { List, ListItem, Typography, Paper } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { List, ListItem, Typography, Paper, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ChannelPage = () => {
   const { channelId } = useParams();
   const [channel, setChannel] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchChannel = async () => {
       try {
-        console.log(channelId)
-        const response = await fetch(`https://localhost:5001/api/Channel/${channelId}`);
+        console.log(channelId);
+        const response = await fetch(
+          `https://localhost:5001/api/Channel/${channelId}`
+        );
         if (!response.ok) {
-          throw new Error('Error fetching channel');
+          throw new Error("Error fetching channel");
         }
         const channelData = await response.json();
         setChannel(channelData);
       } catch (error) {
-        console.error('Error fetching channel:', error);
+        console.error("Error fetching channel:", error);
       }
     };
 
@@ -28,15 +32,31 @@ const ChannelPage = () => {
     return <div>Loading...</div>;
   }
 
+  const navigateToDiscussionPage = (discussionId) => {
+    console.log(discussionId);
+    navigate(`/discussion/${discussionId}`);
+  };
+
   return (
     <div>
       <h2>{channel.name}</h2>
       <List>
-        {channel.discussions.map(discussion => (
+        {channel.discussions.map((discussion) => (
           <ListItem key={discussion.id}>
-            <Paper style={{ marginTop: '10px', padding: '10px', background: '#313331', width: '100%' }}>
-              <Typography style={{ fontWeight: 'bold', color: '#4CAF50'}}>{discussion.title}</Typography>
-              {discussion.messages.map(message => (
+            <Paper
+              style={{
+                marginTop: "10px",
+                padding: "10px",
+                background: "#313331",
+                width: "100%",
+              }}
+            >
+              <Link onClick={() => navigateToDiscussionPage(discussion.id)}>
+                <Typography style={{ fontWeight: "bold", color: "#4CAF50" }}>
+                  {discussion.title}
+                </Typography>
+              </Link>
+              {discussion.messages.map((message) => (
                 <Typography key={message.id}>{message.content}</Typography>
               ))}
             </Paper>
