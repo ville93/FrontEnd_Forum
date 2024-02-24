7d50455ed54fc9f0270f2ba07bd6b05efa3283a0
Ville Ritala <ritalaville@gmail.com>
2024-02-24 11:53:00 +0200

    Possibility to add a new discussion in NewDiscussion component

--- src/components/ChannelList.js
+++ src/components/ChannelList.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { List, ListItem, Typography, Paper, Link } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import WorkIcon from '@mui/icons-material/Work';
-import BusinessIcon from '@mui/icons-material/Business';
-import CelebrityIcon from '@mui/icons-material/EmojiPeople';
-import GamepadIcon from '@mui/icons-material/Gamepad';
-import  PoliticsIcon  from '@mui/icons-material/Balance';
-import CategoryIcon from '@mui/icons-material/Category';
-import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import React, { useState, useEffect } from "react";
+import { List, ListItem, Typography, Paper, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import WorkIcon from "@mui/icons-material/Work";
+import BusinessIcon from "@mui/icons-material/Business";
+import CelebrityIcon from "@mui/icons-material/EmojiPeople";
+import GamepadIcon from "@mui/icons-material/Gamepad";
+import PoliticsIcon from "@mui/icons-material/Balance";
+import CategoryIcon from "@mui/icons-material/Category";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
 
 const ChannelList = ({ onChannelsFetched }) => {
   const [channels, setChannels] = useState([]);
@@ -15,31 +15,31 @@ const ChannelList = ({ onChannelsFetched }) => {
 
   useEffect(() => {
     fetchChannels();
-  }, []); 
+  }, []);
 
   const fetchChannels = () => {
-    fetch('https://localhost:5001/api/Channel')
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://localhost:5001/api/Channel")
+      .then((response) => response.json())
+      .then((data) => {
         setChannels(data);
-        onChannelsFetched(data); 
+        onChannelsFetched(data);
       })
-      .catch(error => console.error('Error fetching channels:', error));
+      .catch((error) => console.error("Error fetching channels:", error));
   };
 
   const getChannelIcon = (channelName) => {
-    switch(channelName.toLowerCase()) {
-      case 'business':
+    switch (channelName.toLowerCase()) {
+      case "business":
         return <BusinessIcon />;
-      case 'celebrity':
+      case "celebrity":
         return <CelebrityIcon />;
-      case 'gaming':
+      case "gaming":
         return <GamepadIcon />;
-      case 'general':
+      case "general":
         return <CategoryIcon />;
-      case 'politics':
+      case "politics":
         return <PoliticsIcon />;
-      case 'sports':
+      case "sports":
         return <SportsSoccerIcon />;
       default:
         return <WorkIcon />;
@@ -47,18 +47,30 @@ const ChannelList = ({ onChannelsFetched }) => {
   };
 
   const handleStartDiscussion = (channelId) => {
-    console.log(channelId)
+    console.log(channelId);
     navigate(`/channel/${channelId}`);
   };
 
   return (
-    <Paper elevation={3} style={{ padding: '10px', background: '#000000', height: '100%', overflowY: 'auto' }}>
+    <Paper
+      elevation={3}
+      style={{
+        padding: "10px",
+        background: "#000000",
+        height: "100%",
+        overflowY: "auto",
+      }}
+    >
       <Typography>CHANNELS</Typography>
       <List>
         {channels.map((channel) => (
           <ListItem key={channel.id}>
             {getChannelIcon(channel.name)}
-            <Link component="button" variant="body1" onClick={() => handleStartDiscussion(channel.id)}>
+            <Link
+              component="button"
+              variant="body1"
+              onClick={() => handleStartDiscussion(channel.id)}
+            >
               <Typography>{channel.name}</Typography>
             </Link>
           </ListItem>
--- src/components/NewDiscussion.js
+++ src/components/NewDiscussion.js
@@ -1,71 +1,126 @@
-import React, { useState } from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
+import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import CloseIcon from "@mui/icons-material/Close";
+import { useNavigate } from "react-router-dom";
 
 const NewDiscussion = ({ channels, onClose }) => {
-  const [channel, setChannel] = useState('');
+  const [title, setTitle] = useState("");
+  const [channel, setChannel] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
   const handleChannelChange = (event) => {
     setChannel(event.target.value);
   };
 
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
   const handleSubmit = () => {
-    onClose();
+    const newDiscussion = {
+      title: title,
+      channelId: channel,
+      messages: [{ content: message }],
+    };
+
+    fetch("https://localhost:5001/api/discussion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDiscussion),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to add discussion");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Discussion added successfully:", data);
+        const discussionId = data.id;
+        navigateToDiscussionPage(discussionId);
+        onClose();
+      })
+      .catch((error) => {
+        console.error("Error adding discussion:", error);
+        onClose();
+      });
+  };
+
+  const navigateToDiscussionPage = (discussionId) => {
+    console.log(discussionId);
+    navigate(`/discussion/${discussionId}`);
   };
 
   return (
-    <Dialog open={true} onClose={onClose}> 
+    <Dialog open={true} onClose={onClose}>
       <DialogContent>
-      <Button
-        color="primary"
-        onClick={onClose}
-        style={{ position: 'absolute', top: 0, right: 0 }}
-      >
-        Close
-      </Button>
-
-      <TextField
-        label="Title"
-        fullWidth
-        variant="outlined"
-        style={{ marginBottom: '16px', marginTop: '16px' }}
-      />
-      <FormControl sx={{ minWidth: 120, marginBottom: '16px' }}>
-        <InputLabel id="demo-simple-select-helper-label">Kanava</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={channel}
-          label="Kanava"
-          onChange={handleChannelChange}
+        <Button
+          color="primary"
+          onClick={onClose}
+          style={{ position: "absolute", top: 0, right: 0 }}
         >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          {channels.map((ch) => (
-            <MenuItem key={ch.id} value={ch.id}>
-              {ch.name}
+          <CloseIcon />
+        </Button>
+
+        <TextField
+          label="Title"
+          fullWidth
+          variant="outlined"
+          style={{ marginBottom: "16px", marginTop: "16px" }}
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <FormControl sx={{ minWidth: 120, marginBottom: "16px" }}>
+          <InputLabel id="demo-simple-select-helper-label">Channel</InputLabel>
+          <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={channel}
+            label="Channel"
+            onChange={handleChannelChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <TextField
-        label="Message"
-        multiline
-        rows={8}
-        fullWidth
-        variant="outlined"
-        style={{ marginBottom: '16px' }}
-      />
-      <Button variant="contained" color="primary" onClick={handleSubmit} style={{ marginTop: '16px' }}>
-        Submit
-      </Button>
+            {channels.map((ch) => (
+              <MenuItem key={ch.id} value={ch.id}>
+                {ch.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField
+          label="Message"
+          multiline
+          rows={8}
+          fullWidth
+          variant="outlined"
+          style={{ marginBottom: "16px" }}
+          value={message}
+          onChange={handleMessageChange}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+          style={{ marginTop: "16px" }}
+        >
+          Submit
+        </Button>
       </DialogContent>
     </Dialog>
   );
